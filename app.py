4c117a5293679ba2477d6a2c5630f5afb71db6f9
Seungho <ksngho7@gmail.com>
2024-10-11 17:23:01 +0900

    db url fix

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ db_url = os.environ.get('DATABASE_URL')
 if db_url and db_url.startswith("postgres://"):
     db_url = db_url.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
